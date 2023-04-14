6575deb19641389df9be89e65a1156a960bdc182
Michael Flückiger <contact@michaelflueckiger.ch>
2023-04-14 08:58:18 +0200

    init

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var socket;
+
+function setup() {
+  createCanvas(400, 400);
+  //socket = socket.io.connect("http;//localhost:3000");
+  socket = io.connect("https://dda-miflck.herokuapp.com/");
+  // Callback function
+  socket.on("mouse", (data) => {
+    console.log("callback from server", data);
+  });
+
+  socket.on("message", (data) => {
+    console.log("message from server", data);
+  });
+
+  // gets called when new client arrives
+  socket.on("client connected", (data) => {
+    console.log("client added", data);
+  });
+
+  background(random(255), 255, 255);
+}
+
+function mouseDragged() {
+  // console.log("Sending: " + mouseX + "," + mouseY);
+
+  var data = {
+    x: mouseX,
+    y: mouseY,
+  };
+  socket.emit("message", data);
+
+  noStroke();
+  fill(0, 255, 0);
+  ellipse(mouseX, mouseY, 20);
+}
